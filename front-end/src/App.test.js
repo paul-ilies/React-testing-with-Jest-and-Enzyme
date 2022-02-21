f5a54e336301131bdfc853bed159265ca76bd689
Ilies Paul Daniel <pauldaniel.ilies@gmail.com>
2022-02-21 14:31:07 +0200

    updated the testing dependencies

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
 import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy();
 });
